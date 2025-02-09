7df4d7936d5b22d67014e2891054d68f7c01ae29
Dae Díaz <diazdae@gmail.com>
2025-02-08 22:15:20 -0600

    feat: added a practice sheet

---
+++ js-02-functions/src/js/practica.js
@@ -0,0 +1,15 @@
+// Realizar una función declarada que sume dos números, retorna el resultado de la suma
+
+function sumarDosNumeros(3,4) {
+    return suma1 + suma2;
+}
+
+console.log( sumarDosNumeros );
+
+// Realizar una función expresada que sume dos números, retorna el resultado de la suma
+
+let sumarNumeros = function (3,4) {
+    return suma 1 + suma2;
+}
+
+console.log( sumarNumeros );
---
+++ js-06-DOM_manipulation/practicas.js
@@ -0,0 +1,7 @@
+const changeElementById = () => {
+
+    const descripcionOperadores = document.getElementById("descripcion-operadores");
+    console.log ( descripcionOperadores );
+}
+
+changeElementById();
