3a64af01e9b696b5b07e22c06d2b5bd79ac3c91f
Dae Díaz <diazdae@gmail.com>
2025-02-11 16:36:06 -0600

    feat: test

---
+++ js-08-OOP/src/modules/api/getProducts/getProducts.js
@@ -0,0 +1,19 @@
+import { generateCardsOfProducts } from "../../card/generateCardsOfProducts.js";
+import { insertCardsToHTML } from "../../card/insertCardsToHTML.js";
+
+
+const getProducts = async( url ) => {
+
+    try {
+        const response = await fetch( url );
+        const products = await response.json();
+        // TODO crear función DTO  cont productsDTO = conversionDTO( products);
+        // TODO const cards = generateCardsOfProducts( productsDTO );
+        const cards = generateCardsOfProducts( products );
+        insertCardsToHTML( cards );            
+    } catch (error) {
+        console.error( error );
+    }
+}
+
+export { getProducts }
